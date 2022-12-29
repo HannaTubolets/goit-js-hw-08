76d91ba734668e8ccfcee29e6192cabdeedcc576
Hanna Tubolets <anna.tubolets@gmail.com>
2022-12-29 20:30:44 +0300

    Added js task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryListEl = document.querySelector('.gallery');
+const galleryItemsEl = [];
+
+galleryItems.forEach(element => {
+
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__link';
+  galleryLink.href = element.original;
+
+  const galleryImg = document.createElement('img');
+  galleryImg.className = 'gallery__image';
+  galleryImg.src = element.preview;
+  galleryImg.setAttribute('title', element.description);
+  galleryImg.alt = element.description;
+
+  galleryLink.append(galleryImg);
+  galleryItemsEl.push(galleryLink);
+});
+
+galleryListEl.append(...galleryItemsEl);
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+});
