1dc4ea3c4b4abab84fa139722465da6ccfd3f333
Hanna Tubolets <anna.tubolets@gmail.com>
2022-12-30 23:18:09 +0300

    Added js-task-03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const userFeedbackFormEl = document.querySelector('.feedback-form');
+const userInfo = {};
+
+const fillFeedbackFormFields = () => {
+  try {
+    const userInfoFromLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    if (userInfoFromLS === null) {
+      return;
+    }
+    console.log(userInfoFromLS);
+
+    for (const prop in userInfoFromLS) {
+      userFeedbackFormEl.elements[prop].value = userInfoFromLS[prop];
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+fillFeedbackFormFields();
+
+const onUserFeedbackFormChange = event => {
+  const { target } = event;
+
+  const fieldValue = target.value;
+  const fieldName = target.name;
+
+  userInfo[fieldName] = fieldValue;
+  console.log(userInfo);
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+};
+
+const onUserFeedbackFormSubmit = event => {
+  event.preventDefault();
+
+  userFeedbackFormEl.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+userFeedbackFormEl.addEventListener(
+    'input',
+    throttle(onUserFeedbackFormChange, 500)
+);
+userFeedbackFormEl.addEventListener('submit', onUserFeedbackFormSubmit);
